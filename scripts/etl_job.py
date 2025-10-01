e613fa71b05e58cbb5ac7751cce3b37e08e348c9
Manikanta varma <msvindukuri@gmail.com>
2025-10-01 15:08:54 -0600

    Update etl_job.py

--- scripts/etl_job.py
+++ scripts/etl_job.py
@@ -1,5 +1,5 @@
 def run_etl():
     print("🚀 Running ETL job...")
-    data = [1, 2, 3]
+    data = [1, 2, 3, 4]
     transformed = [x * 2 for x in data]
     print("✅ Transformed Data:", transformed)
